7305abba5ddca83c162bca4a2b2927d6cc3ea877
HadiaAkbar <hadiaa624@gmail.com>
2026-06-25 21:15:56 +0500

    Delete ai_resume_screening_system directory

--- ai_resume_screening_system/run_app.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-if __name__ == "__main__":
-    # Path to the streamlit app
-    app_path = os.path.join("src", "ui", "app.py")
-    
-    # Run the streamlit command
-    subprocess.run(["streamlit", "run", app_path])
